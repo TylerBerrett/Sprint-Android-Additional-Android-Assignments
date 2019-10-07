adcf8bdba1c14610d87f55c513dd9a9d3d8753b1
Tyler Berrett <btylerberrett@gmail.com>
2019-10-07 13:06:02 -0600

    initial commit

--- M01/Guided Project/MediaProgramming/app/src/main/java/com/lambdaschool/mediaprogramming/MainActivity.kt
+++ M01/Guided Project/MediaProgramming/app/src/main/java/com/lambdaschool/mediaprogramming/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //TODO 1: Disable the play/pause button by default (you'll enable it later)
-
+        play_pause_button.isEnabled = false
         //TODO 2: Add the play/pause functionality including the animation for the button and
         //TODO playing or pausing the VideoView.
         playOrPauseFunctionality()
@@ -29,6 +29,7 @@ class MainActivity : AppCompatActivity() {
         //TODO 3: Add functionality to listen to seekbar drag events to set videoview to the
         //TODO appropriate part of the video.
         seekBarFunctionality()
+
     }
 
     private fun playOrPauseFunctionality() {
@@ -36,25 +37,61 @@ class MainActivity : AppCompatActivity() {
         // If the video is not playing, start it
         // else pause the video
         // Start the animation
+        play_pause_button.setOnClickListener {
+            if (video_view.isPlaying) {
+                video_view.pause()
+                play_pause_button.setImageDrawable(getDrawable(R.drawable.avd_anim_pause_play))
+                //pause
+            } else {
+                //play
+                video_view.start()
+                play_pause_button.setImageDrawable(getDrawable(R.drawable.avd_anim_play_pause))
+            }
+            val drawable = play_pause_button.drawable
+            if (drawable is Animatable) {
+                drawable.start()
+            }
+        }
     }
 
     private fun seekBarFunctionality() {
         // In the SeekBar listener, when the seekbar progress is changed,
         // update the video progress
+        video_seek_bar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                seekBar?.let {
+                    video_view.seekTo(it.progress)
+                }
+
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {}
+
+        })
     }
 
     override fun onStart() {
         super.onStart()
 
         //TODO 4: Set the video URI to the videoview (This video is an mp4 file in the raw folder )
+        video_view.setVideoURI(Uri.parse("android.resource://" + packageName + "/" + R.raw.live_views_of_starman))
 
         //TODO 5: Set an onPreparedListener to the videoview and enable the play/pause button in the
         //TODO callback
+        video_view.setOnPreparedListener {
+            play_pause_button.isEnabled = true
+            video_seek_bar.max = it.duration
+            //video_seek_bar.progress = it.currentPosition
+
+        }
     }
 
     //TODO 8: Pause the video when onStop is called, if user navigates away from the screen
     override fun onStop() {
         super.onStop()
+        video_view.pause()
     }
 
     //TODO 7: Notice that we create an overflow menu to handle navigation between activities
