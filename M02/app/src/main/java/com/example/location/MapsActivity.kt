e6a59f625e45c05e1830874bbac4fd1b8c4ca9d5
Tyler Berrett <btylerberrett@gmail.com>
2019-10-08 16:41:12 -0600

    add request location permissions add center button add create pin button

--- M02/app/src/main/java/com/example/location/MapsActivity.kt
+++ M02/app/src/main/java/com/example/location/MapsActivity.kt
@@ -1,7 +1,16 @@
 package com.example.location
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -9,10 +18,17 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_maps.*
+import kotlin.system.exitProcess
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    private lateinit var  centerLatLong: LatLng
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +37,17 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // request the permission
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 5)
+        } else {
+            fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        }
     }
+
+
+
 
     /**
      * Manipulates the map once available.
@@ -35,9 +61,27 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
+
+
+        fusedLocationClient.lastLocation.addOnSuccessListener {
+            val currentLocation = LatLng(it.latitude, it.longitude)
+            centerLatLong = currentLocation
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation))
+        }
+
+        center.setOnClickListener {
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(centerLatLong))
+        }
+
+        marker.setOnClickListener {
+            mMap.addMarker(MarkerOptions().position(mMap.cameraPosition.target).title("My marker"))
+        }
+
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
+        /*val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))*/
     }
+
+
 }
